63c368d698a0397f191bf38b04d50a567d79cc82
JammingJack <amineseth1@gmail.com>
2021-04-02 00:54:08 +0100

    added redirect activity and log in functionality

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText usrNameInput;
+    EditText pwdInput;
+    String usr;
+    String pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        usrNameInput = findViewById(R.id.editTextTextPersonName);
+        pwdInput = findViewById(R.id.editTextTextPassword);
+    }
+    public void envoyer(View view){
+        Intent intent = new Intent(this, MainActivity2.class);
+
+        usr = usrNameInput.getText().toString();
+        pwd = pwdInput.getText().toString();
+        intent.putExtra("usr",usr);
+        intent.putExtra("pwd",pwd);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/tp2/MainActivity2.java
+++ app/src/main/java/com/example/tp2/MainActivity2.java
@@ -2,13 +2,31 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
-
+    TextView usrText;
+    TextView pwdText;
+    String usr;
+    String pwd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        usrText = findViewById(R.id.redirectUsrLabel);
+        pwdText = findViewById(R.id.redirectPwdLabel);
+        Intent intent = getIntent();
+        usr = intent.getStringExtra("usr");
+        pwd = intent.getStringExtra("pwd");
+        usrText.setText(usrText.getText().toString() + usr);
+        pwdText.setText(pwdText.getText().toString() + pwd);
+    }
+
+    public void retour(View view){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
