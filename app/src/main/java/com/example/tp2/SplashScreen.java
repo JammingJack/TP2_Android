ca11fa5d8135a9a0a55ccebf91a31125be33a56c
JammingJack <amineseth1@gmail.com>
2021-04-02 02:05:08 +0100

    deleted manuel programatical config of emulator sys lang

--- app/src/main/java/com/example/tp2/SplashScreen.java
+++ app/src/main/java/com/example/tp2/SplashScreen.java
@@ -3,8 +3,11 @@ package com.example.tp2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.os.Handler;
+
+import java.util.Locale;
 
 public class SplashScreen extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 4000;
@@ -12,6 +15,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
